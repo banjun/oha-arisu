a855393099bb5b84321720d46630a2ab474842f9
banjun <banjun@gmail.com>
2018-06-06 09:16:47 +0900

    Initial Commit

---
+++ Good Morning Arisu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Good Morning Arisu
+//
+//  Created by BAN Jun on 2018/06/06.
+//  Copyright © 2018 BAN Jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
